cbf541dc49754d304725236db8261cbff11acb72
taurimenas <taurimenas01@gmail.com>
2020-03-14 22:03:08 +0200

    Created Db class

---
+++ Bot/Db.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot
+{
+    class Db
+    {
+        public int testNumber = 5;
+        
+        public Db(String data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
--- Bot/Program.cs
+++ Bot/Program.cs
@@ -24,9 +24,17 @@ namespace Bot
             var fElementText = await page.WaitForSelectorAsync("#search > div.s-desktop-width-max.s-desktop-content.sg-row > div.sg-col-20-of-24.sg-col-28-of-32.sg-col-16-of-20.sg-col.sg-col-32-of-36.sg-col-8-of-12.sg-col-12-of-16.sg-col-24-of-28 > div > span:nth-child(6) > div.s-result-list.s-search-results.sg-row > div:nth-child(1) > div > span > div > div > div > div > div:nth-child(2) > div.sg-col-4-of-12.sg-col-8-of-16.sg-col-16-of-24.sg-col-12-of-20.sg-col-24-of-32.sg-col.sg-col-28-of-36.sg-col-20-of-28 > div > div:nth-child(1) > div > div > div:nth-child(1) > h2 > a > span");
             var txt1 = (await fElementText.GetPropertyAsync("textContent")).ToString();
             var txt2 = await fElementText.EvaluateFunctionAsync<string>("t => t.textContent", fElementText);
-            Console.WriteLine(txt1);
-            Console.WriteLine(txt2);
+            //Console.WriteLine(txt1);
+            //Console.WriteLine(txt2);
+
             //TODO 1
+
+            //TEST DB
+            Db dataBase = new Db(txt2);
+            Console.WriteLine(dataBase.testNumber);
+
+            //-------
+
         }
     }
 }
